24ab4acf90c373b89e37933da514613487c60950
Abdel ADAM <adamdjamal18@gmail.com>
2019-05-27 10:00:00 +0100

    feat: pagination custom

--- core/core/settings.py
+++ core/core/settings.py
@@ -34,6 +34,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'core.pagination.StandardResultsSetPagination',
+    'PAGE_SIZE': 20,
 }
 
 
