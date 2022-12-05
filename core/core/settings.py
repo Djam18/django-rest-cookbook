33451bd20fddddd9c1d312d32f6170e4d6e77ccd
Abdel ADAM <adamdjamal18@gmail.com>
2022-12-05 10:00:00 +0100

    chore: Django 4.2 LTS

--- core/core/settings.py
+++ core/core/settings.py
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
-USE_L10N = True
+USE_L10N = True  # deprecated in 4.0, removed in 5.0 - keeping for now
 USE_TZ = True
 
 STATIC_URL = '/static/'
